c2f59ba30f7f05c99a53e42d3f1a1bfd1f5b815e
Shiyaam J O <iamshiyaaam7@gmail.com>
2025-09-14 13:42:43 +0530

    Created Patient Service with H2 in memory DB

---
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,44 @@
+package com.pm.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+
+//    private LocalDate registeredDate; // remove @NotNull here
+//
+//    // This will be called automatically before insert
+//    @PrePersist
+//    protected void onCreate() {
+//        this.registeredDate = LocalDate.now();
+//    }
+}
